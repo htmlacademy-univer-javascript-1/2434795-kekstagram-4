99128fa4ba1cdcfcff7bf2077b79946a0fd95f43
Keks <keks@htmlacademy.ru>
2023-11-30 19:56:31 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/function.js
@@ -0,0 +1,10 @@
+function check_length_string(str, length){
+  if (str.length <= length){
+    return true;
+  }
+  return false;
+}
+
+function is_palindrome(str){
+  return str === str.split('').reverse().join('');
+}

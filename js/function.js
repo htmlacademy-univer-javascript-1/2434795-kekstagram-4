4f837befb1272b42d3b274b0e6b48c1ec4277b22
Алексей Стахнев <alexeyst18082004@gmail.com>
2023-12-19 04:20:28 +0500

    Задание 5.16

--- js/function.js
+++ js/function.js
@@ -8,3 +8,22 @@ function checkLengthString(str, length){
 function isPalindrome(str){
   return str.toLowerCase() === str.toLowerCase().split('').reverse().join('');
 }
+
+function meetingIsCorrect(begin, end, startMeeting, durationMeeting){
+  const beginArray = inputTimeToArray(begin);
+  const endArray = inputTimeToArray(end);
+  const startMeetingArray = inputTimeToArray(startMeeting);
+  const durationMeetingInHours = Math.floor(durationMeeting/60);
+  const durationMeetingInMinutes = durationMeeting%60;
+  const endOfMeetingArrray = [startMeetingArray[0] + durationMeetingInHours, startMeetingArray[1] + durationMeetingInMinutes];
+
+  if (startMeetingArray[0] < beginArray[0]) {return false;}
+  if (startMeetingArray[0] == beginArray[0] && startMeetingArray[1] < beginArray[1]) {return false;}
+  if (endOfMeetingArrray[0] == endArray[0] && endOfMeetingArrray[1] > endArray[1]) {return false;}
+  if (endOfMeetingArrray[0] > endArray[0]) {return false;}
+  return true;
+}
+
+function inputTimeToArray(inputTime){
+  return inputTime.split(':').map(Number);
+}

b5e449fd8391a0ebe840454c6264e4f57f4201cc
Алексей Стахнев <alexeyst18082004@gmail.com>
2023-12-23 05:27:23 +0500

    Задание 8.15

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-import './function.js';
-import {createPhotoDescription} from './data.js';
-import {renderThumbnail} from './render-thumbnails.js';
-import {addEventListenerToPicture} from './render-big-picture.js';
-
-const picture = createPhotoDescription();
-renderThumbnail(picture);
-addEventListenerToPicture(picture);
+import { drawSmall } from './draw.js';
